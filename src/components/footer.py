5e28a48d1937f2bb67b1b8f5e6ea0cf5ada76457
SUMIT KUMAR JAISWAL <166597425+Sumitjaiswal9334@users.noreply.github.com>
2026-07-18 22:39:19 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,25 +1,31 @@
 import streamlit as st
+import base64
+from pathlib import Path
+
+
+def get_base64(image_name):
+    image_path = Path(__file__).parent / image_name
+    with open(image_path, "rb") as img:
+        return base64.b64encode(img.read()).decode()
 
 
 def footer_home():
-    logo_url = "https://www.image2url.com/r2/default/images/1783441882417-3282c0fb-443e-4d8b-923c-4f374f1f89e5.png"
-    
+    logo_url = f"data:image/png;base64,{get_base64('name_banner.png')}"
+
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:4px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:40px ; margin-left:-5px' />
+        <div style="margin-top:2rem; display:flex; gap:4px; justify-content:center; align-items:center;">
+            <p style="font-weight:bold; color:white;">Created with ❤️ by</p>
+            <img src="{logo_url}" style="max-height:40px; margin-left:-5px;" />
         </div>
-                
-                """, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
 
 
 def footer_dashboard():
-    logo_url = "https://www.image2url.com/r2/default/images/1783441882417-3282c0fb-443e-4d8b-923c-4f374f1f89e5.png"
-    
+    logo_url = f"data:image/png;base64,{get_base64('name_banner.png')}"
+
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:4px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:40px; margin-left:-5px;' />
+        <div style="margin-top:2rem; display:flex; gap:4px; justify-content:center; align-items:center;">
+            <p style="font-weight:bold; color:black;">Created with ❤️ by</p>
+            <img src="{logo_url}" style="max-height:40px; margin-left:-5px;" />
         </div>
-                
-                """, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
